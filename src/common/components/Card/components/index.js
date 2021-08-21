d83f8196b3fd7fd99d73c1310998576566614501
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-08-21 22:50:45 +0700

    Update index.js

--- src/common/components/Card/components/index.js
+++ src/common/components/Card/components/index.js
@@ -1,10 +1,15 @@
+import classNames from 'classnames';
 import React from 'react';
 
-const Card = ({ header, children, footer }) => {
+const Card = ({ header, children, footer, className }) => {
 	return (
-		<div className="card shadow-sm mb-4">
+		<div
+			className={classNames('card shadow-sm mb-4', {
+				[className]: className
+			})}
+		>
 			{header && <div className="card-header">{header}</div>}
-			<div className="card-body bg-white">{children}</div>
+			<div className="card-body">{children}</div>
 			{footer && <div className="card-footer">{footer}</div>}
 		</div>
 	);
